a39a3e5c62cf6c2187d487ece70e910d9ab8e504
Luke Sonnet <luke.sonnet@gmail.com>
2018-06-27 12:44:52 +0500

    Update cleaning scriptt

--- 01_clean_scrutiny_data.R
+++ 01_clean_scrutiny_data.R
@@ -2,8 +2,15 @@
 # NOTES: check INVESTIGATE lines for things to check against raw pdfs, TODO for unfinished sections
 # ----------
 
-candidate_import <- read.csv("data/candidate_scrutiny_forms_2018.csv", na.strings = "")
+library(tidyverse)
+
+# CC: Why do you have na.strings = ""?
+candidate_import <- read.csv("data/candidate_scrutiny_forms_2018.csv", na.strings = "", stringsAsFactors = FALSE)
 candidate_df <- candidate_import
+
+# ----------
+# Clean candidate metadata
+# ----------
 
 # years as factors ------------------------------------------------------------
 
@@ -126,7 +133,7 @@ candidate_df$candidate_code[candidate_df$province == "Sindh" &
 
 # strip dashes from CNIC / MNIC numbers ---------------------------------------
 # for reference SBP CNIC pattern is 12345-1234567-1, MNIC pattern 123-12-123456, latter may also have alphumerics
-#
+
 candidate_df$candidate_CNIC_SBP <- as.factor(gsub("-", "", candidate_df$candidate_CNIC_SBP))
 candidate_df$candidate_MNIC_NAB <- as.factor(gsub("NULL", NA, candidate_df$candidate_MNIC_NAB))
 candidate_df$candidate_MNIC_SBP <- as.factor(gsub("-", "", candidate_df$candidate_MNIC_SBP))
@@ -138,6 +145,10 @@ candidate_df$candidate_name_SBP <- gsub("NA", NA, candidate_df$candidate_name_SB
 candidate_df$candidate_NTN_issue <- gsub("NA", NA, candidate_df$candidate_NTN_issue)
 candidate_df$candidate_tax_remarks <- gsub("NA", NA, candidate_df$candidate_tax_remarks)
 candidate_df$candidate_tax_paid <- as.factor(gsub("NA", NA, candidate_df$candidate_tax_paid))
+
+# ----------
+# Clean up tax data
+# ----------
 
 # INVESTIGATE - ok to set '-' as NA?
 candidate_df$candidate_tax_receipts <- as.factor(gsub("-", NA, candidate_df$candidate_tax_receipts))
@@ -169,9 +180,107 @@ candidate_df$candidate_RTO <- gsub("RTO \\(Corporate\\) Lahore", "RTO (CORPORATE
 candidate_df$candidate_RTO <- gsub("RTO FSD", "RTO FAISALABAD", candidate_df$candidate_RTO)
 candidate_df$candidate_RTO <- gsub("RTO, Mn", "RTO MULTAN", candidate_df$candidate_RTO)
 
-# clean up tax + NAB + loan remarks -------------------------------------------
+# clean up tax remarks -------------------------------------------
 
-# TODO
+tax_vars <- c("candidate_tax_income", "candidate_tax_receipts", "candidate_tax_paid")
+candidate_df <- candidate_df %>%
+  mutate_at(tax_vars,
+            funs(gsub("\\,|\\s+", "", .))) %>%
+  mutate_at(tax_vars,
+            funs(num = as.numeric(.)))
+
+
+na_if_no_match <- function(.x) {
+  if (length(.x) == 0) NA else sum(as.numeric(.x))
+}
+for (tax_var in tax_vars) {
+  na_num <- is.na(candidate_df[[paste0(tax_var, "_num")]])
+  candidate_df[[tax_var]][na_num] <-
+    map_dbl(
+      str_extract_all(candidate_df[[tax_var]][na_num], 
+                      "\\d+(\\.\\d{2})?"), 
+      na_if_no_match
+    )
+}
+
+# ----------
+# Clean up NAB comments
+# ----------
+
+# table(candidate_df$candidate_NAB_status)
+candidate_df <- candidate_df %>%
+  mutate(
+    candidate_NAB_guilty = ifelse(
+      grepl(
+        "It is certified that no information about( any)? conviction or plea bargain",
+        candidate_NAB_status
+      ),
+      "No Conviction or Plea Bargain",
+      "Conviction or Plea Bargain"
+    ),
+    candidate_NAB_conviction = ifelse(
+      grepl(
+        "It is certified that information about conviction",
+        candidate_NAB_status
+      ),
+      "Conviction",
+      "No conviction"
+    ),
+    candidate_NAB_plea = ifelse(
+      grepl(
+        "It is certified that information about [pP]lea [bB]argain",
+        candidate_NAB_status
+      ),
+      "Plea bargain",
+      "No plea bargain"
+    ),
+    candidate_NAB_remarks = str_extract(
+      candidate_NAB_status,
+      "(?<=Remarks\\: ).*?(?=\\sShakeel)"
+    ),
+    # Following isn't perfect
+    candidate_NAB_accused = ifelse(
+      grepl("is( an)? accused|pending", candidate_NAB_remarks),
+      "Accused or pending",
+      "Not accused or pending"
+    )
+  )
+
+# with(candidate_df, table(candidate_NAB_conviction, candidate_NAB_guilty, candidate_NAB_plea))
+# table(candidate_df$candidate_NAB_remarks)
+# with(candidate_df, table(candidate_NAB_accused, candidate_NAB_conviction))
+# with(candidate_df, table(candidate_NAB_accused, candidate_NAB_guilty))
+# candidate_df$candidate_NAB_status[candidate_df$candidate_NAB_guilty == "Conviction or Plea Bargain" & candidate_df$candidate_NAB_conviction == "No conviction"]
+# candidate_df$candidate_NAB_remarks[candidate_df$candidate_NAB_guilty == "Conviction or Plea Bargain" & candidate_df$candidate_NAB_accused == "Not accused"]
+# unique(candidate_df$candidate_NAB_remarks[candidate_df$candidate_NAB_guilty == "No Conviction or Plea Bargain" & candidate_df$candidate_NAB_accused == "Not accused"])
+# candidate_df$candidate_NAB_remarks[candidate_df$candidate_NAB_guilty == "Conviction or Plea Bargain" & candidate_df$candidate_NAB_accused == "Not accused"]
+# unique(candidate_df$candidate_NAB_status[candidate_df$candidate_NAB_guilty == "Conviction or Plea Bargain"])
+
+# ----------
+# SBP cleaning
+# ----------
+
+# table(table(candidate_df$candidate_loan_info))
+# table(candidate_df$candidate_loan_info)[table(candidate_df$candidate_loan_info) > 20000] # nada
+# table(candidate_df$candidate_loan_info)[table(candidate_df$candidate_loan_info) == 8616] # nada
+# table(candidate_df$candidate_loan_info)[table(candidate_df$candidate_loan_info) > 3 & table(candidate_df$candidate_loan_info) < 8000] # nada
+# table(table(candidate_df$candidate_loan_info))
+
+temp_loan <-
+  gsub(
+    "\\(Rs\\. in Million\\)\\s+CNIC\\/Name\\s+Relation( with)?\\s+(FI )?Name( of Company\\s+FI Name)?\\s+Overdue\\s+Writeoff (Candidate|with)?",
+    "",
+    candidate_df$candidate_loan_info
+  ) %>%
+  gsub(
+    "Further\\, candidate/spouse/dependent is also director\\/owner of following companies having overdue \\/write off amounting to Rs 2 million and above for last one year",
+    "",
+    .
+  ) %>%
+  str_split("\\:\\-") %>%
+  map_dfr(~ data.frame(candidate_personal_loan = trimws(.x[1]), candidate_business_loan = trimws(.x[2])))
+candidate_df <- bind_cols(candidate_df, temp_loan)
+# TODO actually extract loan data
 
 # check if FBR-NAB names match ------------------------------------------------
 
@@ -200,6 +309,8 @@ candidate_df <- candidate_df %>% mutate(
 )
 
 # TODO INVESTIGATE mismatches (just 9 unique cases)
+# CC: btw here is a nice way to open some pdfs quickly
+map(candidate_df$target[candidate_df$MNIC_match == "MNIC Mismatch"], open)
 
 # establish candidate UIDs to identify multi-constituency contestants ------------
 
@@ -240,9 +351,13 @@ candidate_df <- dplyr::select(candidate_df,
   candidate_MNIC_NAB, candidate_MNIC_SBP, candidate_NTN,
   candidate_name_FBR, candidate_name_NAB, candidate_name_SBP,
   candidate_tax_type, candidate_NTN_issue, candidate_RTO,
-  candidate_tax_paid, candidate_tax_receipts, candidate_tax_income, candidate_tax_remarks,
-  candidate_NAB_status, 
-  candidate_loan_info,
+  candidate_tax_paid, candidate_tax_paid_num, 
+  candidate_tax_receipts, candidate_tax_receipts_num,
+  candidate_tax_income, candidate_tax_income_num,
+  candidate_tax_remarks,
+  candidate_NAB_guilty, candidate_NAB_conviction, candidate_NAB_plea, candidate_NAB_accused,
+  candidate_NAB_remarks,
+  candidate_personal_loan, candidate_business_loan,
   multi_candidate, 
   urdu_name_match, 
   CNIC_FBR_NAB_match, CNIC_FBR_SBP_match, CNIC_NAB_SBP_match,
