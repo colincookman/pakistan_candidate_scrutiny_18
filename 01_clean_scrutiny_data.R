3e480c2a0385938dc12957522d8fcf065a250b17
colincookman <ccookman@gmail.com>
2018-06-27 17:04:59 -0400

    Further refinements to cleaning code, updated Readme summaries

--- 01_clean_scrutiny_data.R
+++ 01_clean_scrutiny_data.R
@@ -4,9 +4,23 @@
 
 library(tidyverse)
 
-# CC: Why do you have na.strings = ""?
-candidate_import <- read.csv("data/candidate_scrutiny_forms_2018.csv", na.strings = "", stringsAsFactors = FALSE)
+candidate_import <- read.csv("data/scraped_scrutiny_forms.csv", stringsAsFactors = FALSE)
 candidate_df <- candidate_import
+
+# ----------
+# Cut duplicate data
+# ----------
+# ECP included duplicate copies of the following KPK Provincial Assembly candidate filings in the KPK National Assembly folder - cutting from list
+# PK-1-27, PK-23-11, PK-52-21, PK-6-25, PK-83-25
+
+candidate_df <- subset(candidate_df, !(candidate_df$assembly == "National Assembly" & grepl("PK-", candidate_df$constituency_number)))
+
+# The PP-9 folder is also housing lots of duplicate data folders, including some from totally different constituencies -- all of these appear to
+# exist in their proper homes, so removing these instances
+
+temp <- filter(candidate_df, constituency_number == "PP-9")
+temp <- filter(temp, is.na(temp$candidate_number))
+candidate_df <- candidate_df[!(candidate_df$target %in% temp$target), ]
 
 # ----------
 # Clean candidate metadata
@@ -33,19 +47,26 @@ candidate_df$constituency_number[grepl("NASW", candidate_df$constituency_number)
 # at least in available ECP data national assembly minority candidates don't have provincial affiliations
 candidate_df$province <- gsub("Minority", "National", candidate_df$province)
 
+# one candidate for PS-87 is buried in a subfolder and did not get properly assigned their candidate number, correcting
+candidate_df$candidate_number[candidate_df$target == "data/2018 Candidate Scrutiny Forms/Sindh/Provincial Assembly/PS-87/PS-87/PS-87-0018_4250114748170"] <- "18"
+
 # reclaim ECP candidate numbers for minority / women's candidates, just for listing purposes
 candidate_df$candidate_number <- as.character(candidate_df$candidate_number)
 
 candidate_df$candidate_number[candidate_df$constituency_number == "Womens List"] <- 
-  str_match(str_split(candidate_df$target[candidate_df$constituency_number == "Womens List"], "/", simplify = TRUE)[,5], "(?<=\\-)(.*?)(?=\\_)")[,1]
+  str_match(str_split(candidate_df$target[candidate_df$constituency_number == "Womens List"], "/", simplify = TRUE)[,6], "(?<=\\-)(.*?)(?=\\_)")[,1]
 
 candidate_df$candidate_number[candidate_df$constituency_number == "Minority List"] <- 
-  str_match(str_split(candidate_df$target[candidate_df$constituency_number == "Minority List"], "/", simplify = TRUE)[,5], "(?<=\\-)(.*?)(?=\\_)")[,1]
+  str_match(str_split(candidate_df$target[candidate_df$constituency_number == "Minority List"], "/", simplify = TRUE)[,6], "(?<=\\-)(.*?)(?=\\_)")[,1]
 
 candidate_df$candidate_number[candidate_df$constituency_number == "Minority List" & candidate_df$province == "National"] <- 
-  str_match(str_split(candidate_df$target[candidate_df$constituency_number == "Minority List" & candidate_df$province == "National"], "/", simplify = TRUE)[,4], "(?<=\\-)(.*?)(?=\\_)")[,1]
+  str_match(str_split(candidate_df$target[candidate_df$constituency_number == "Minority List" & candidate_df$province == "National"], "/", simplify = TRUE)[,5], "(?<=\\-)(.*?)(?=\\_)")[,1]
 
-# concatenate a candidate-constituency code
+# convert candidate numeric to numeric for constitency (remove prefix 0s for WL / ML)
+
+candidate_df$candidate_number <- as.numeric(candidate_df$candidate_number)
+
+# concatenate a constituency-candidate number code
 candidate_df <- candidate_df %>% mutate(
   candidate_code = paste(candidate_df$constituency_number, 
       as.numeric(as.character(candidate_df$candidate_number)), sep = "-")
@@ -131,6 +152,11 @@ candidate_df$candidate_code[candidate_df$province == "Sindh" &
                                                              candidate_df$constituency_number == "Womens List"]
   )
 
+
+# pull ECP subfolder CNIC listings to compare with FBR/NAB/SBP reports --------
+
+candidate_df$candidate_CNIC_ECP <- str_split(candidate_df$target, "_", simplify = TRUE)[,2]
+
 # strip dashes from CNIC / MNIC numbers ---------------------------------------
 # for reference SBP CNIC pattern is 12345-1234567-1, MNIC pattern 123-12-123456, latter may also have alphumerics
 
@@ -150,7 +176,7 @@ candidate_df$candidate_tax_paid <- as.factor(gsub("NA", NA, candidate_df$candida
 # Clean up tax data
 # ----------
 
-# INVESTIGATE - ok to set '-' as NA?
+# convert '-' instances to NA values
 candidate_df$candidate_tax_receipts <- as.factor(gsub("-", NA, candidate_df$candidate_tax_receipts))
 candidate_df$candidate_tax_receipts <- as.factor(gsub("NA", NA, candidate_df$candidate_tax_receipts))
 candidate_df$candidate_tax_income <- as.factor(gsub("-", NA, candidate_df$candidate_tax_income))
@@ -282,6 +308,11 @@ temp_loan <-
 candidate_df <- bind_cols(candidate_df, temp_loan)
 # TODO actually extract loan data
 
+
+# ----------
+# Data checks and comparisons
+# ----------
+
 # check if FBR-NAB names match ------------------------------------------------
 
 candidate_df <- candidate_df %>% mutate(
@@ -290,17 +321,23 @@ candidate_df <- candidate_df %>% mutate(
 
 # many apparent name mismatches - on quick visual scan source appears to mostly be typos or nonstandardized name spelling
 
-# check if FBR-NAB-SBP CNICs match --------------------------------------------
-# INVESTIGATE -- there are pdf encoding issues with these CNIC strings - string reading / copying from original pdfs is not correct
+# check if ECP-FBR-NAB-SBP CNICs match --------------------------------------------
+# There are pdf encoding issues with FBR CNIC strings - string reading / copying from original pdfs is not correct when compared to visual read
 
 candidate_df <- candidate_df %>% mutate(
+  CNIC_ECP_FBR_match = ifelse(as.character(candidate_CNIC_ECP) != as.character(candidate_CNIC_FBR), "CNIC Mismatch", "CNIC Match"),
+  CNIC_ECP_NAB_match = ifelse(as.character(candidate_CNIC_ECP) != as.character(candidate_CNIC_NAB), "CNIC Mismatch", "CNIC Match"),
+  CNIC_ECP_SBP_match = ifelse(as.character(candidate_CNIC_ECP) != as.character(candidate_CNIC_SBP), "CNIC Mismatch", "CNIC Match"),
   CNIC_FBR_NAB_match = ifelse(as.character(candidate_CNIC_FBR) != as.character(candidate_CNIC_NAB), "CNIC Mismatch", "CNIC Match"),
   CNIC_FBR_SBP_match = ifelse(as.character(candidate_CNIC_FBR) != as.character(candidate_CNIC_SBP), "CNIC Mismatch", "CNIC Match"),
   CNIC_NAB_SBP_match = ifelse(as.character(candidate_CNIC_NAB) != as.character(candidate_CNIC_SBP), "CNIC Mismatch", "CNIC Match")
 )
 
-# in almost all cases issue appears to be FBR not matching with NAB/SBP due to encoding errors - 
-# only two unique cases of NAB-SBP CNICs not matching (PP-32-1 and PP-32-2, visual check of FBR + NAB forms shows FBR-SBP versions match, NAB the outlier)
+# ECP and SBP CNICs match in all cases;
+# Only two unique cases of ECP/SBP-NAB CNICs not matching 
+# (PP-32-1 and PP-32-2, a visual check of FBR + NAB forms shows FBR-SBP versions also match, NAB the outlier)
+# 16787 out of 19538 FBR CNICs don't match with ECP - this issue appears to be due to encoding errors 
+# that are preventing an accurate parse of the number in the pdf
 
 # check that NAB-SBP MNICs match ----------------------------------------------
 
@@ -314,66 +351,96 @@ map(candidate_df$target[candidate_df$MNIC_match == "MNIC Mismatch"], open)
 
 # establish candidate UIDs to identify multi-constituency contestants ------------
 
-uniques <- candidate_df[!duplicated(candidate_df$candidate_code), 1:30]
-duplicate_SBP_CNIC <- uniques[(duplicated(uniques$candidate_CNIC_SBP) | duplicated(uniques$candidate_CNIC_SBP, fromLast = TRUE)), 1:30]
+# just the first tax row for each candidacy filing
+unique_filings <- candidate_df[!duplicated(candidate_df$candidate_code), 1:44]
+# just the first instance of any individual candidate's filing in any constituency
+unique_ECP_CNIC <- unique_filings[!duplicated(unique_filings$candidate_CNIC_ECP), 1:44]
+# identify all cases amongst candidacy filings where there are matching CNICs
+multiple_CNIC_filings <- unique_filings[(duplicated(unique_filings$candidate_CNIC_ECP) | duplicated(unique_filings$candidate_CNIC_ECP, fromLast = TRUE)), 1:44]
 
 # identify candidates that contested multiple constituencies, or otherwise candidates whose names do not match despite sharing identical CNICs
-duplicate_SBP_CNIC$candidate_name_FBR <- as.character(duplicate_SBP_CNIC$candidate_name_FBR)
-duplicate_SBP_CNIC$candidate_name_NAB <- as.character(duplicate_SBP_CNIC$candidate_name_NAB)
-duplicate_SBP_CNIC$candidate_CNIC_SBP <- as.character(duplicate_SBP_CNIC$candidate_CNIC_SBP)
+multiple_CNIC_filings$candidate_name_FBR <- as.character(multiple_CNIC_filings$candidate_name_FBR)
+multiple_CNIC_filings$candidate_name_NAB <- as.character(multiple_CNIC_filings$candidate_name_NAB)
+multiple_CNIC_filings$candidate_CNIC_SBP <- as.character(multiple_CNIC_filings$candidate_CNIC_SBP)
 
-duplicate_SBP_CNIC <- dplyr::arrange(duplicate_SBP_CNIC, desc(candidate_CNIC_SBP))
+multiple_CNIC_filings <- dplyr::arrange(multiple_CNIC_filings, desc(candidate_CNIC_ECP))
 
-duplicate_SBP_CNIC <- duplicate_SBP_CNIC %>% group_by(candidate_CNIC_SBP) %>% 
+multiple_CNIC_filings <- multiple_CNIC_filings %>% group_by(candidate_CNIC_ECP) %>% 
   mutate(
     multi_candidate_FBR = ifelse(candidate_name_FBR == lag(candidate_name_FBR), "Multi-Candidate", "Mismatch"),
     multi_candidate_NAB = ifelse(candidate_name_NAB == lag(candidate_name_NAB), "Multi-Candidate", "Mismatch")
     )
 
-# multiple typos in FBR names are throwing mismatches but only NAB name mismatch (candidate_code PS-35-3) is due to first name / last name reversal
-# otherwise all repeat SBP CNICs in dataset are the same candidate contesting multiple races
+# multiple typos in FBR names are throwing mismatches but the only NAB name mismatch (candidate_code PS-35-3) is due to first name / last name reversal
+# otherwise all repeat ECP CNICs in dataset are the same candidate (checked against NAB name) contesting in multiple races
 # unnecessary to create new UIDs since CNICs are in fact unique
 
-multi_candidate <- dplyr::select(duplicate_SBP_CNIC, candidate_CNIC_SBP)
+multi_candidate <- dplyr::select(multiple_CNIC_filings, candidate_CNIC_ECP)
 multi_candidate <- unique(multi_candidate)
 multi_candidate$multi_candidate <- "YES"
 
-candidate_df <- left_join(candidate_df, multi_candidate, by = "candidate_CNIC_SBP")
+candidate_df <- left_join(candidate_df, multi_candidate, by = "candidate_CNIC_ECP")
 
-# TODO -- confirm to see if this joined properly - 2751 unique multi-candidate CNICs but seeing 2752 uniques, 1 NA introduced somehow?
-summary(unique(as.factor(duplicate_SBP_CNIC$candidate_CNIC_SBP)))
-summary(unique(as.factor(candidate_df$candidate_CNIC_SBP[candidate_df$multi_candidate == "YES"])))
+# ----------
+# Final output
+# ----------
 
 # reorder columns -------------------------------------------------------------
 candidate_df <- dplyr::select(candidate_df,
-  candidate_code, province, assembly, constituency_number, candidate_number,
-  tax_year,
-  candidate_CNIC_FBR, candidate_CNIC_NAB, candidate_CNIC_SBP,
-  candidate_MNIC_NAB, candidate_MNIC_SBP, candidate_NTN,
-  candidate_name_FBR, candidate_name_NAB, candidate_name_SBP,
-  candidate_tax_type, candidate_NTN_issue, candidate_RTO,
-  candidate_tax_paid, candidate_tax_paid_num, 
-  candidate_tax_receipts, candidate_tax_receipts_num,
-  candidate_tax_income, candidate_tax_income_num,
-  candidate_tax_remarks,
-  candidate_NAB_guilty, candidate_NAB_conviction, candidate_NAB_plea, candidate_NAB_accused,
-  candidate_NAB_remarks,
-  candidate_personal_loan, candidate_business_loan,
-  multi_candidate, 
-  urdu_name_match, 
-  CNIC_FBR_NAB_match, CNIC_FBR_SBP_match, CNIC_NAB_SBP_match,
-  MNIC_match,
-  target
+# constituency meta
+candidate_code, province, assembly, constituency_number, candidate_number,
+# candidate meta
+candidate_CNIC_ECP, # dropping candidate_CNIC_FBR, candidate_CNIC_NAB, and candidate_CNIC_SBP
+multi_candidate,
+candidate_NTN, 
+candidate_MNIC_NAB, candidate_MNIC_SBP,
+# tax data
+tax_year,
+candidate_tax_type, candidate_NTN_issue, candidate_RTO,
+candidate_tax_paid, candidate_tax_paid_num, 
+candidate_tax_receipts, candidate_tax_receipts_num,
+candidate_tax_income, candidate_tax_income_num,
+candidate_tax_remarks,
+# NAB data
+candidate_NAB_guilty, candidate_NAB_conviction, candidate_NAB_plea, candidate_NAB_accused,
+candidate_NAB_remarks,
+# SBP data
+candidate_personal_loan, candidate_business_loan,
+# additional metadata
+candidate_name_FBR, candidate_name_NAB, candidate_name_SBP,
+urdu_name_match, 
+MNIC_match,
+target
 )
 
 candidate_df <- dplyr::arrange(candidate_df, candidate_code)
 
-# TODO -- possible variables to drop from final:
-# FBR and NAB CNICs if we trust SBP to be most accurate
-# CNIC_ and MNIC_ match checks
+# TODO -- possible other variables to drop from final:
+# MNIC_ match checks
 # FBR Urdu name (multiple apparent match errors with NAB)
 # urdu_name_match if we drop FBR
-# SBP name depending on transliteration results of NAB name
+
+# summary counts --------------------------------------------------------------
+
+constituency_filing_count <- unique_filings %>% group_by(province, assembly, constituency_number) %>%
+  summarize(
+    count = n()
+  )
+write.csv(constituency_filing_count, file = "data/constituency_filing_count.csv", row.names = FALSE)
+
+aggregate_filing_count <- constituency_filing_count %>% group_by(province, assembly) %>%
+  summarize(
+    direct_seats = sum(count[constituency_number != "Womens List" & constituency_number != "Minority List"]),
+    womens_seats = sum(count[constituency_number == "Womens List"]),
+    minority_seats = sum(count[constituency_number == "Minority List"])
+  )
+
+# search for possible missing candidate forms in available sequence -----------
+# TODO
+
+candidate_lists <- candidate_df %>% group_by(constituency_number) %>% dplyr::select(candidate_code, candidate_number)
+candidate_lists <- candidate_lists[!duplicated(candidate_lists$candidate_code), 1:3]
+
 
 
 # re-write csv for final output -----------------------------------------------
--- README.md
+++ README.md
@@ -2,17 +2,58 @@
 
 This repository hosts open source R code used to scan, consolidate, tidy, and clean candidate scrutiny forms released by the Election Commission of Pakistan for prospective candidates for Pakistan's 2018 national and provincial assembly elections. The final output after cleaning is also included as [pk_candidate_scrutiny_data_2018.csv](https://github.com/colincookman/pakistan_2018_candidates/blob/master/pk_candidate_scrutiny_data_2018.csv).
 
-Please note that as of current writing this code is still a work in progress and data outputs hosted here are still incomplete. For questions, suggestions, or to contribute, please leave an issue here or contact the contributors, Luke Sonnet and Colin Cookman.
+For questions, suggestions, or to contribute, please leave an issue here or contact the contributors, Luke Sonnet and Colin Cookman.
 
 ## Data
 The raw files used as a data source are approximately 10.3 GB in size and too large to host in this Github repository; the [data subfolder](https://github.com/colincookman/pakistan_2018_candidates/tree/master/data) provides information on alternative mirror hosts that can be used to obtain these original raw pdfs.
 
-## Variable key
-TODO
-
-## Analysis
+## Code replication
 
 Once the data are downloaded to `data/2018 Candidate Scrutiny Forms` following the instructions in the [data subfolder](https://github.com/colincookman/pakistan_2018_candidates/tree/master/data), then you can:
 
 * Run `00_scrape_scrutiny_forms.R` to read from PDFs and create intermediary data
 * Run `01_clean_scrutiny_data.R` to clean up and standardize the scraped data
+
+## Scope and possible gaps in data
+Pakistani election law does not impose residency requirements for candidacy filings and allows individual candidates to contest multiple seats simultaneously, within and across assemblies and provinces. As of the initial data release the ECP provided information for 19397 candidacy filings and 15904 unique candidates (as identified by accompanying Computerized National ID Card records). 
+
+### Candidacy filing counts
+| province    | assembly            | direct_seats | womens_seats | minority_seats | 
+|-------------|---------------------|--------------|--------------|----------------| 
+| Balochistan | National Assembly   | 373          | 40           | NA             | 
+| Balochistan | Provincial Assembly | 1302         | 13           | 24             | 
+| KPK         | National Assembly   | 1080         | 90           | NA             | 
+| KPK         | Provincial Assembly | 1959         | 177          | 59             | 
+| National    | National Assembly   | NA           | NA           | 215            | 
+| Punjab      | National Assembly   | 2426         | 172          | NA             | 
+| Punjab      | Provincial Assembly | 6234         | 668          | 141            | 
+| Sindh       | National Assembly   | 1181         | 178          | NA             | 
+| Sindh       | Provincial Assembly | 2915         | 94           | 56             | 
+
+(A constituency-level summary of candidacy filing reports can be found [here]([pk_candidate_scrutiny_data_2018.csv](https://github.com/colincookman/pakistan_2018_candidates/blob/master/constituency_filing_count.csv) in the data folder.)
+
+The ECP had [previously reported](https://www.ecp.gov.pk/PrintDocument.aspx?PressId=55295&type=Image) on June 18 2018 ([Wayback Ref](https://web.archive.org/web/20180627164802/https://www.ecp.gov.pk/PrintDocument.aspx?PressId=55295&type=Image)) that 21482 nomination papers had been filed. While we cannot account for the discrepancy or identify missing candidates at this stage, based on the ECP's earlier aggregate figures the following number of candidacy filings may be missing from the current dataset (or not accounted for in the earlier ECP statement in cases where more records were available):
+
+### ECP reported filings (Difference with available data)
+| province    | assembly            | direct_seats | womens_seats | minority_seats | 
+|-------------|---------------------|--------------|--------------|----------------| 
+| Balochistan | National Assembly   | 435 (-62)    | 36 (+4)      | NA             | 
+| Balochistan | Provincial Assembly | 1400 (-98)   | 116 (-103)   | 56 (-32)       | 
+| KPK         | National Assembly   | 992 (+88)    | 88 (+2)      | NA             | 
+| KPK         | Provincial Assembly | 1920 (+39)   | 262 (-85)    | 73 (-14)       | 
+| National    | National Assembly   | NA           | NA           | 154 (+61)      | 
+| Punjab      | National Assembly   | 2700 (-274)  | 236 (-64)    | NA             | 
+| Punjab      | Provincial Assembly | 6747 (-513)  | 664 (+4)     | 232 (-91)      | 
+| Sindh       | National Assembly   | 1346 (-165)  | 76 (+102)    | NA             | 
+| Sindh       | Provincial Assembly | 3626 (-711)  | 213 (-119)   | 110 (-54)      | 
+
+
+A final list of candidates following the process of scrutiny, disqualification, appeal, and withdrawal is currently [scheduled for release](https://www.ecp.gov.pk/PrintDocument.aspx?PressId=55262&type=Image) by the ECP on June 30 2018 which may allow for some retroactive analysis to identify missing filing data.
+
+
+## Output variable key
+TODO
+
+
+## Caveats
+This dataset is being presented to encourage broader open data sharing among the community of analysts on Pakistan. We make no guarantees as to and cannot verify the accuracy of, or account for any discrepancies in, the underlying data.
